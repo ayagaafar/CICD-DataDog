5e7cd6a126d85c0c8fd060ce3367cfe962028e6e
ayagaafar <31242225+ayagaafar@users.noreply.github.com>
2019-12-16 00:09:03 +0200

    Update WebApplication.java

--- app/src/main/java/com/hellokoding/springboot/WebApplication.java
+++ app/src/main/java/com/hellokoding/springboot/WebApplication.java
@@ -5,6 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WebApplication {
+        @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        // Register resource handler for images
+        registry.addResourceHandler("/images/**").addResourceLocations("/WEB-INF/images/")
+                .setCacheControl(CacheControl.maxAge(2, TimeUnit.HOURS).cachePublic());
+    }
+    
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
     }
